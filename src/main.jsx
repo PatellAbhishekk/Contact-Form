fae0ca0b107e718c845b610f96170dc553053cfd
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2024-12-09 14:32:34 +0000

    Create pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import Home from "./pages/Home";
 
-const appEl = document.querySelector("#App");
+const appEl = document.querySelector("#app");
 const root = createRoot(appEl);
+
 root.render(
   <React.StrictMode>
-    <Duster />
+    <Home />
   </React.StrictMode>,
 );
-
-function Duster() {
-  return (
-    <div>
-      <h1 className="font-bold text-zinc-900 hover:text-red-600 md:text-4xl">
-        Duster
-      </h1>
-    </div>
-  );
-}
