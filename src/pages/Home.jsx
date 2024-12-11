bc2a9945ac88668fcebe1d31c8eadd84deacd799
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2024-12-11 14:30:33 +0000

    checkbox

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@ import Form from "@/components/Form";
 
 function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-green-400 to-blue-400 px-4 py-3 md:px-8">
+    <div className="min-h-screen px-4 py-3 md:px-8">
       <Header />
-      <main className="grid grid-cols-8 bg-red-100">
-        <div className="col-span-2 hidden rounded-lg bg-green-300 md:block">
+      <main className="grid grid-cols-8">
+        <div className="col-span-2 hidden rounded-lg md:block">
           <Aside />
         </div>
         <div className="col-span-full rounded-lg bg-blue-300 p-5 md:col-span-6">
